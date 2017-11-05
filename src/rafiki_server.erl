ca19b0dfdbec54937748832f62fcc825b4623eab
Dairon Medina Caro <dairon@gooee.com>
2017-11-05 14:49:58 -0500

    Add callback_mode specs

--- src/rafiki_server.erl
+++ src/rafiki_server.erl
@@ -4,7 +4,9 @@
 -export([start/2, stop/1]).
 -export([init/1, handle_event/4, code_change/4, terminate/3]).
 
-callback_mode() ->
-    state_functions.
+-spec callback_mode() -> gen_statem:callback_mode_result().
+callback_mode() -> state_functions.
+
+
 
 
